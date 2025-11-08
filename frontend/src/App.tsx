a4c5894447361e8d53f240c29b3b80b3a1e7a70c
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2025-11-08 13:33:57 -0700

    +landing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import LandingScreen from "./Screens/LandingScreen";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <LandingScreen />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 };
 
 export default App;
